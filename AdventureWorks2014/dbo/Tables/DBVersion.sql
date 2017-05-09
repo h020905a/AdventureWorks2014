02b740ccf5f74a936a4bd838998294924799adb3
Edric Howard <Edric.Howard@Ticketmaster.co.uk>
2017-05-09 16:26:34 +0100

    adding a table for versioning

---
+++ AdventureWorks2014/dbo/Tables/DBVersion.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[DBVersion](
+	[DBV_ref] [int] IDENTITY(1,1) NOT NULL,
+	[DBV_previousversion] [varchar](50) CONSTRAINT [D_DBVersion$DBV_previousversion] DEFAULT ('') NULL,
+	[DBV_currentversion] [varchar](50)  NOT NULL,
+	[DBV_date] [datetime] CONSTRAINT [D_DBVersion$DBV_date]  DEFAULT (getdate()) NOT NULL,
+	[DBV_octopusreleaseno] [varchar](10) CONSTRAINT [D_DBVersion$DBV_octopusreleaseno] DEFAULT ('') NOT NULL, 
+    CONSTRAINT [PK_DBVersion$DBV_ref] PRIMARY KEY NONCLUSTERED ([DBV_ref] ASC) 
+);
